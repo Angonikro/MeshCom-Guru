92eff9ef0f744ee45849901874ddce8cddcb1fc7
Angonikro <98713317+Angonikro@users.noreply.github.com>
2026-09-20 19:01:25 +0200

    Windows GPU Test

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 import sys
 
-# Raspberry-Pi-sichere QtWebEngine-Einstellung: GPU/Vulkan-Compositing
-# wird nur für Chromium abgeschaltet. Qt Quick/RHI-Umgebungsvariablen werden
-# bewusst NICHT gesetzt, weil sie auf älteren X11-Systemen selbst OpenGL-
-# Initialisierungsfehler verursachen können.
-os.environ.setdefault(
-    "QTWEBENGINE_CHROMIUM_FLAGS",
-    "--disable-gpu --disable-gpu-compositing --disable-vulkan "
-    "--disable-features=Vulkan,UseSkiaRenderer"
-)
+# Raspberry-Pi-sichere QtWebEngine-Einstellung.
+# Nur Linux/Raspberry Pi verwendet die bisherigen Chromium-Abschaltungen.
+# Unter Windows darf QtWebEngine die vorhandene GPU/Vulkan-Unterstützung nutzen.
+if sys.platform.startswith("linux"):
+    os.environ.setdefault(
+        "QTWEBENGINE_CHROMIUM_FLAGS",
+        "--disable-gpu --disable-gpu-compositing --disable-vulkan "
+        "--disable-features=Vulkan,UseSkiaRenderer"
+    )
 
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
